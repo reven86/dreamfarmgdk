f3e1a33af567c3bda474bea94e8a48f22330df02
Andrew Karpushin <reven86@gmail.com>
2010-08-19 21:35:41 +0000

    added dx8 sdk, dx9 sdk (summer 2004), openmp libs

---
+++ Source/third_party/openmp-vc90/ompassem.h
@@ -0,0 +1,24 @@
+/***
+*ompassem.h - Libraries Assembly information
+*
+*       Copyright (c) Microsoft Corporation. All rights reserved.
+*
+*Purpose:
+*       This file has information about Libraries Assembly version.
+*
+*
+****/
+
+#pragma once
+
+#ifndef _VC_ASSEMBLY_PUBLICKEYTOKEN
+#define _VC_ASSEMBLY_PUBLICKEYTOKEN "1fc8b3b9a1e18e3b"
+#endif
+
+#ifndef __OPENMP_ASSEMBLY_VERSION
+#define __OPENMP_ASSEMBLY_VERSION "9.0.21022.8"
+#endif
+
+#ifndef __OPENMP_ASSEMBLY_NAME_PREFIX
+#define __OPENMP_ASSEMBLY_NAME_PREFIX "Microsoft.VC90"
+#endif
